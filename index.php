20737485eacf127ef11bec4a943ba800c7b2e44a
Connor <connmal01@gmail.com>
2019-01-25 13:34:49 +0000

    Aleteration added slide 9 back

--- index.php
+++ index.php
@@ -332,6 +332,23 @@
         .text("We will now ask a some questions about yourself. Please click \"Next\" after answering each question to continue.")
         .call(wrap, textWidth)
 
+        var slide_9= d3.select("svg").append("g")
+          .attr("id", "slide9")
+          .style("display", "none")
+        slide_9.append("rect")
+          .style("fill", "white")
+          .attr("class", "slide")
+          .attr("x", 0)
+          .attr("y", 0)
+          .attr("width", bodyWidth)
+          .attr("height", bodyHeight)
+        slide_9.append("text")
+          .attr("class", "slideText")
+          .attr("x", center - (textWidth / 2))
+          .attr("y", text_offset_top)
+          .text("Thank you for participating, you may close this broswer window.")
+          .call(wrap, textWidth)
+
 
 
       // Boxes indicating frequency into which nodes are dragged (4, 5 or 6 categories)
@@ -2509,11 +2526,18 @@
               .attr("y", text_offset_top + 40)
               .text("Click \"Next\" to end the survey.")
               .call(wrap, textWidth);
+              document.getElementById('silde9').style.display = "block";
 
-            // Release window close-prevention
+            currSlide++;
           }
-            unhook();
+        } else if (currSlide == 47) {
+              document.getElementById("slide9").style.display = "none";
+
+// Release window close-prevention
+
+              unhook();
         }
+
         $('#Next').blur();
       }
 
