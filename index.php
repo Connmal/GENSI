8ff4ec89cd515dd92a866838b01c78cd254026a1
Connor <connmal01@gmail.com>
2019-01-29 22:31:29 +0000

    got rid of "

--- index.php
+++ index.php
@@ -182,7 +182,7 @@
         .attr("x", 0)
         .attr("y", 0)
         .attr("width", bodyWidth )
-        .attr("height", "bodyHeight");
+        .attr("height", bodyHeight);
       slide_1.append("text")
         .attr("class", "slideText")
         .attr("x", center - textWidth / 2)
